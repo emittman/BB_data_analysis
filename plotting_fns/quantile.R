f0f185330acffc3273461b52e62d5ad06502f96a
emittman <emittman@iastate.edu>
2017-02-10 12:50:25 -0600

    Update bfcn, add to outliine

--- plotting_fns/quantile.R
+++ plotting_fns/quantile.R
@@ -1,6 +1,6 @@
 ## Load In Samples ##
 
-s <- readRDS("workflow/samples_2_1.rds")
+s <- readRDS("samples_2_1.rds")
 samp <- extract(s)
 
 ## Get Quantiles from GFLP Model ##
@@ -16,20 +16,22 @@ quant <- function(loc1, loc2, scl1, scl2, pi, b, t) {
   }
 
 #Function to Get Quantiles of Interest for All Posterior Draws.  Takes Model Argument and Quantile.
-bfcn <- function(model, b){
+bfcn <- function(model, b, quantiles){
     j <- model 
     Qp <- sapply(1:24000, function(i){
           uniroot(function(t) quant(samp$mu1[i,j],samp$mu2[i,j],samp$sigma1[i,j],samp$sigma2[i,j], exp(samp$log_pi[i,j]), b, t),c(0,300000))$root
   })
-  q <- quantile(Qp, c(.025,.5,.975))
-  data.frame(model=j,lower=q[1],med=q[2],upper=q[3])
+  q <- quantile(Qp, quantiles)
+  df <- data.frame(model=j,t(q))
+  rownames(df) <- NULL
+  names(df)[-1] <- sapply(quantiles, function(q) as.character(round(q,3)))
+  df
 }
 
 #Run this Function for All Models.  Note:  Should be a quick way to combine this with first function.
 df <- NULL
 for (j in 1:21){
   df <- rbind(df,bfcn(j,.1))
-  rownames(df) <- NULL
 }
 
 
