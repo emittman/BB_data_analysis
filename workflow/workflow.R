2667b7ed4f1ed27351fb6795816b3391066cada2
Eric Mittman <emittman@iastate.edu>
2017-03-08 10:19:27 -0600

    Add samples from reduced model with relaxed priors

--- workflow/workflow.R
+++ workflow/workflow.R
@@ -6,7 +6,7 @@ datasets = commands(
 
 analyses = analyses(
   commands = commands(
-    samples_logodds_reduced_2_21 = run_mcmc_logodds(..dataset.., chains = 6, iter = 5500, warmup = 1500)
+    samples_logodds_reduced_2_21 = run_mcmc_logodds(..dataset.., chains = 4, iter = 4000, warmup = 1000)
   ),
   datasets = datasets
 )
