31006951c2188c5752b369bf27a997bbe52776ee
emittman <emittman@iastate.edu>
2017-03-10 13:30:13 -0600

    Relax filter for drive models

--- workflow/workflow.R
+++ workflow/workflow.R
@@ -1,7 +1,8 @@
 library(remakeGenerator)
 
 datasets = commands(
-  data_2_21 = prepare_data(lb_fails = 5, lb_late_fails = 1, lb_early_fails = 1)
+  data_2_21 = prepare_data(lb_fails = 5, lb_late_fails = 1, lb_early_fails = 1),
+  data_3_10 = prepare_data(lb_fails = 3, lb_late_fails = 0, lb_early_fails = 0)
 )
 
 analyses = analyses(
