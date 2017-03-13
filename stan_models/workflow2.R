ef102cef6fc360509e8b934ecb0bbfecb929b552
emittman <emittman@iastate.edu>
2017-03-13 14:34:33 -0500

    Add two Stan models: one with no pooling (expecting problems with sampling for several drive models), one with increasingly relaxed priors, including no upper bound on sigma2[m]

---
+++ stan_models/workflow2.R
@@ -0,0 +1,16 @@
+library(remakeGenerator)
+
+datasets = commands(
+  data_2_21 = prepare_data(lb_fails = 5, lb_late_fails = 1, lb_early_fails = 1)
+)
+
+analyses = analyses(
+  commands = commands(
+    samples_logodds_nopool_3_13 = run_mcmc_logodds_nopool(..dataset.., chains = 4, iter = 3000, warmup = 1500)
+  ),
+  datasets = datasets
+)
+
+targets = targets(datasets = datasets, analyses = analyses)
+
+workflow(targets, sources = "functions.R", packages = "rstan")
--- workflow/functions.R
+++ workflow/functions.R
@@ -63,6 +63,30 @@ run_mcmc_logodds <- function(dataset, chains = 4, iter = 10, warmup = iter/2, p 
   # return(stan_data)
 }
 
+run_mcmc_logodds_nopool <- function(dataset, chains = 4, iter = 10, warmup = iter/2, p = c(.5,.2)){
+  require(rstan)
+  rstan_options(auto_write = TRUE)
+  options(mc.cores = parallel::detectCores())#format for Stan
+  stan_data <- with(dataset,
+                    list(M = length(unique(model)),
+                         N_obs = sum(!censored),
+                         N_cens = sum(censored),
+                         starttime_obs = log(starttime[!censored]+1),
+                         starttime_cens = log(starttime[censored]+1),
+                         endtime_obs = log(endtime[!censored]+1),
+                         endtime_cens = log(endtime[censored]+1),
+                         dm_obs = model[!censored],
+                         dm_cens = model[censored],
+                         p = p)
+  )
+  
+  
+  m <- stan_model(file = "../stan_models/glfp_no_pooling.stan")
+  s <- sampling(obj = m, data = stan_data, chains = chains, iter = iter, warmup = warmup, control = list(adapt_delta = .999), init=0)
+  return(s)
+  # return(stan_data)
+}
+
 count_divergences <- function(fit, chain, inc_warmup) {
   sampler_params <- get_sampler_params(fit, inc_warmup=inc_warmup)
   sum(sapply(sampler_params, function(x) c(x[,'divergent__']))[,chain])
