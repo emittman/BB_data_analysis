772acbe0a75b3b78d3759a472aa83d5628ebd231
Colin <clewisbe@iastate.edu>
2017-04-16 14:03:11 -0500

    loo function for full model.  Need to add flexibility for other models.

--- loo/loofctn.R
+++ loo/loofctn.R
@@ -1,13 +1,14 @@
 #LOO Model Comparison#
 library(rstan)
+library(loo)
 
 s <- readRDS("MCMC_draws/samples_lor_only3fails.rds")
 samp <- extract(s)
 
 
-#Make Matrix of Draws to Pass to Function
 
 
+#Data for Loo
 d <- readRDS("BB_data/clean_unit_summaries.rds")
 d$model <- as.integer(d$model)
 d$starttime <- log(d$starttime + 1)
@@ -30,47 +31,50 @@ sev_logccdf <- function(y, mu, sigma){
 
 
 #Function to Take an Observation and Return a Vector of the Likelihood over all Posterior Parameters
+#Need to Add Arguments for Different Models
 
 llfun <- function(i, data, draws){
-  dat <- data[i,]
-  model <- dat$model
-  ll <- vector(length=10)
-  for (j in 1:10){
-  #Get Parameter Estimates  
-  mu1 <- draws$mu1[j]
-  sigma1 <- draws$sigma1[j]
-  log_pi <- draws$log_pi[j]
-  mu2 <- draws$mu2[j,model]
-  sigma2 <- draws$sigma2[j,model]
-  if (dat$failed==0){
-    #log(p * f1 * (1 - F2) + f2 * (1 - p * F1))
-    likenum  <-  log(exp(log_pi + sev_logpdf(dat$endtime, mu1, sigma1) +
+  #dat <- data[i,]
+  model <- data$model
+  ll <- vector(length=24000)
+  for (j in 1:24000){
+    #Get Parameter Estimates  
+    mu1 <- draws$mu1[j]
+    sigma1 <- draws$sigma1[j]
+    log_pi <- draws$log_pi[j]
+    mu2 <- draws$mu2[j,model]
+    sigma2 <- draws$sigma2[j,model]
+        if (dat$failed==0){
+            #log(p * f1 * (1 - F2) + f2 * (1 - p * F1))
+            likenum  <-  log(exp(log_pi + sev_logpdf(dat$endtime, mu1, sigma1) +
                                sev_logccdf(dat$endtime, mu2, sigma2)) + 
                              exp(sev_logpdf(dat$endtime, mu2, sigma2) + 
                                log(1-exp(log_pi + sev_logcdf(dat$endtime, mu1, sigma1)))))
     
-    #log(1 - p * F1) + log(1 - F2)
-    likedem <- log(1-exp(log_pi + sev_logcdf(dat$starttime, mu1, sigma1))) + 
-      sev_logccdf(dat$starttime, mu2, sigma2)
-    l <- likenum - likedem                
-  }
+            #log(1 - p * F1) + log(1 - F2)
+            likedem <- log(1-exp(log_pi + sev_logcdf(dat$starttime, mu1, sigma1))) + 
+            sev_logccdf(dat$starttime, mu2, sigma2)
+            l <- likenum - likedem                
+          }
+        else{
+        #log(1 - p * F1) + log(1 - F2)
+        likenum = log(1-exp(log_pi + sev_logcdf(dat$endtime, mu1, sigma1))) + 
+        sev_logccdf(dat$endtime, mu2, sigma2)
   
-  else{
+        #log((1 - p * F1) * (1 - F2))
+        likedem = log(1-exp(log_pi + sev_logcdf(dat$starttime, mu1, sigma1))) + 
+        sev_logccdf(dat$starttime, mu2, sigma2)
     
-    #log(1 - p * F1) + log(1 - F2)
-    likenum = log(1-exp(log_pi + sev_logcdf(dat$endtime, mu1, sigma1))) + 
-      sev_logccdf(dat$endtime, mu2, sigma2)
-    
-    #log((1 - p * F1) * (1 - F2))
-    likedem = log(1-exp(log_pi + sev_logcdf(dat$starttime, mu1, sigma1))) + 
-      sev_logccdf(dat$starttime, mu2, sigma2)
-    
-    l <- likenum - likedem 
-  }
-  ll[j] <- l
-  }
+        l <- likenum - likedem 
+          }
+      ll[j] <- l
+    }
   return(ll)
 }
 
 test <- llfun(1, data = d,draws = samp)
+
+N <- nrow(d)
+log_like_mat <- sapply(1:N, function(i) llfun(i,d[i,,drop=FALSE], samp))
+loo_with_mat <- loo(log_like_mat)
     
