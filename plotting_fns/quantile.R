99c636d84f9ccd985949df78aff713abd235b5ce
Colin <clewisbe@iastate.edu>
2017-05-02 19:07:14 -0500

    Paper edits and plots.

--- plotting_fns/quantile.R
+++ plotting_fns/quantile.R
@@ -1,7 +1,8 @@
 ## Load In Samples ##
+library(rstan)
 
 # s <- readRDS("reduce_relaxed.rds")
-s <- readRDS("samples_2_1.rds")
+s <- readRDS("MCMC_draws/samples_lor_only3fails.rds")
 samp <- extract(s)
 
 ## Get Quantiles from GFLP Model ##
@@ -34,7 +35,7 @@ bfcn2 <- function(model, b, samp, quantiles = c(.025, .5, .975)){
   
   j <- model 
   Qp <- sapply(1:dim(samp$lp__), function(i){
-    uniroot(function(t) quant(samp$mu1[i],samp$mu2[i,j],samp$sigma1[i],samp$sigma2[i,j], exp(samp$log_pi[i,j]), b, t),c(0,300000))$root
+    uniroot(function(t) quant(samp$mu1[i],samp$mu2[i,j],samp$sigma1[i],samp$sigma2[i,j], exp(samp$log_pi[i,j]), b, t),c(1,3000000))$root
   })
   q <- quantile(Qp, quantiles)
   df <- data.frame(model=j,t(q))
@@ -50,8 +51,34 @@ for (j in 1:21){
 }
 
 df <- NULL
-for (j in 1:21){
-  df <- rbind(df,bfcn(j,.1, samp))
+for (j in 1:44){
+  df <- rbind(df,bfcn2(j,.1, samp))
 }
+
+
+
+#B10_Model4.rds is in paper folder for future use
+b10 <- readRDS("B10_Model4.rds")
+colnames(b10) <- c("model","lb","med","ub")
+b10$lb <- b10$lb/(24*365)
+b10$med <- b10$med/(24*365)
+b10$ub <- b10$ub/(24*365)
+
+
+#Make Catepillar Plot for B10; Perhaps Sort by Sample Size?
+p <- ggplot(b10, aes(x=as.factor(model), y=med, ymin=lb, ymax=ub)) +
+  geom_pointrange() +
+  coord_flip() +
+  xlab('Drive Model') + theme_bw() + 
+  ylab("Years")   +
+  scale_x_discrete(breaks=seq(1,44,1)) + 
+p
+
+
+
+
+
+
+
 
 xtable::xtable(cbind(df, df_reduced), digits = 0)
